f556f044b263a011e79fb9e8f12afc0e41f6e29e
stephane-vanraes <stephane.vanraes@gmail.com>
2022-03-30 16:43:47 +0200

    this is ok

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,35 +1,30 @@
 import JSZip from 'jszip';
 import hooksContent from '$lib/content/hooks';
-
 import buildFolder from '$lib/builders/buildFolder';
 
 export async function post({ request }) {
-	try {
-		const data: App.Project = await request.json();
-		const zip = new JSZip();
+    const data : App.Project = await request.json();
+    const zip = new JSZip();
 
-		if (data.hooks) {
-			zip.file('hooks.js', hooksContent);
-		}
+    if (data.hooks) {
+        zip.file('hooks.js', hooksContent)
+    }
 
-		buildFolder(data.routes, zip, []);
+    buildFolder(data.routes, zip, [])
 
-		const file = await zip.generateAsync({
-			type: 'nodebuffer',
-			compression: 'STORE'
-		});
+    const file = await zip.generateAsync({ 
+        type: 'nodebuffer',
+        compression: 'DEFLATE',
+        compressionOptions: {
+            level: 9
+        }
+    })
 
-		return {
-			status: 200,
-			body: file,
-			headers: {
-				'Content-Type': 'application/zip'
-			}
-		};
-	} catch (err) {
-		return {
-			status: 500,
-			body: err
-		};
-	}
+    return {
+        status: 200,
+        body: file,
+        headers: {
+            "Content-Type": "application/zip"
+        }
+    }
 }
